790d517dc96fbe4b0f977ca605fe2b015ea5cad3
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-06-09 14:30:30 +0200

    finished moving through directories when clicking a folder, navigating with the breadcrumb menu, and opening files (default navigator mode)

--- index.php
+++ index.php
@@ -12,124 +12,134 @@
 
     <?php
 
-    /* ----------- USEFUL DIRECTORY INFORMATIONS ----------- */
-    echo '<header>';
-    echo '<div class="directory-container">';
-    // name of the host server, e.g. my.local
-    echo '<h2>server name: <span class="directory-info">' . $_SERVER['SERVER_NAME'] . '</span></h2><br>';
-    // filename of the currently executing script
-    echo '<h2>script name with php self: <span class="directory-info">' . $_SERVER['PHP_SELF'] . '</span></h2><br>';
-    // current directory, e.g. files-explorer
-    echo '<h2>script folder #1: <span class="directory-info">' . basename(getcwd()) . '</span></h3><br>';
-    // current directory, e.g. files-explorer
-    echo '<h2>script folder #2: <span class="directory-info">' . basename(dirname(__FILE__)) . '</span></h2><br>';
-    // absolute path, e.g. /Applications/MAMP/htdocs/files-explorer/index.php
-    $path = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME'];
-    echo '<h2>absolute path: <span class="directory-info">' . $path . '</span></h2><br>';
-    // site's root path including the script, e.g. /Applications/MAMP/htdocs/files-explorer/index.php
-    echo '<h2>script directory: <span class="directory-info">' . realpath(__FILE__) . '</span></h2><br>';
-    // site's root path without the script's filename, e.g. /Applications/MAMP/htdocs/files-explorer/
+    // get absolute path
     define('ROOT_DIR', realpath(__DIR__));
-    echo '<h2>root directory: <span class="directory-info">' . ROOT_DIR . '</span></h2><br>';
+
+    echo '<header>';
+    echo '<div class="header-container">';
+    // server protocol
+    $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+    // domain name
+    $domain = $_SERVER['SERVER_NAME'];
+    // concatenate all variables to get the complete base URL
+    $base_url = "${protocol}://${domain}";
+    // display url
+    echo "<h4>server root: <span class=\"directory-info\">$base_url</span></h3>";
+    echo '</div>';
+
+    /* ----------- CHANGE DIRECTORY ----------- */
+    echo '<div class="current-directory-container">';
+    // check if a folder has been clicked
+    if (isset($_POST['selected_item'])) {
+      $selected_item = $_POST['selected_item']; // store folder path
+      // check whether to change the current directory or not
+      if (chdir($selected_item)) {
+        chdir($selected_item);
+      } else {
+        chdir(getcwd());
+      }
+    }
+    /* ----------- CHECK CURRENT DIRECTORY ----------- */
+    // remove '/' from path and split it into individual items
+    $cwd = explode(DIRECTORY_SEPARATOR, getcwd());
+    // check whether the cwd is different than the virtualhost
+    if (getcwd() !== ROOT_DIR) {
+      $last_directory = $cwd[sizeof($cwd) - 1]; // always get the last directory of the path
+      $parent_directory = DIRECTORY_SEPARATOR . $last_directory . DIRECTORY_SEPARATOR;
+      echo "<h4>parent directory: <span class=\"directory-info\">$parent_directory</span></h4><br>";
+    } else {
+      $parent_directory = DIRECTORY_SEPARATOR; // if it's the virtualhost, prepend just a slash
+    }
+    /* --------------------------------- */
+
+    /* ----------- FORMAT SERVER URL ----------- */
+    echo '<div class="current-directory-container">';
+    // absolute path without script folder
+    $base_dir = dirname(ROOT_DIR);
+    // format url for files
+    $url = str_replace($base_dir, $base_url, $selected_item);
+    echo "<h2>url: <span class=\"directory-info\">$url</span></h2>";
     echo '</div>';
     echo '</header>';
     /* --------------------------------- */
 
+    /* ----------- NAVBAR MENU ----------- */
+    echo '<div class="navbar-container">';
+    $cwd_accum = ''; // initialize increment
+    // iterate over root directory
+    foreach ($cwd as $item) {
+      $cwd_accum = $cwd_accum . $item . DIRECTORY_SEPARATOR; // recursive path increment
+      echo '<form method="post" enctype="application/x-www-form-urlencoded">';
+      echo "<input type=\"hidden\" name=\"selected_item\" value=\"" . $cwd_accum . "\">";
+      echo "<button type=\"submit\">$item</button>";
+      echo "</form>";
+    }
+    echo '</div>';
+    /* --------------------------------- */
+
+    /* ----------- DISPLAY CURRENT DIRECTORY ITEMS ----------- */
     echo '<main>';
-
-    /* ----------- CHANGE DIRECTORY ----------- */
-    echo '<section><div class="current-directory-container">';
-    // check whether a folder has been clicked
-    if (isset($_POST['selected_item'])) {
-      // store <form> passed value
-      $selected_item = $_POST['selected_item'];
-      echo '<h4>selected item: <span class="directory-info">' . $selected_item . '</span></h4><br>';
-      // check whether to move to a new directory or not
-      if (chdir($selected_item)) {
-        chdir($selected_item);
-        echo '<h4>directory successfully changed!</h4><br>';
-      } else {
-        chdir(getcwd());
-        // $selected_item = getcwd();
-        echo '<h4>failed to change directory!</h4><br>';
-      }
-    }
-    /* --------------------------------- */
-
-    /* ----------- CHECK CURRENT DIRECTORY ----------- */
-    // check whether cwd is different than the virtual host
-    if (getcwd() !== ROOT_DIR) {
-      // remove '/' from path and split it into individual items
-      $cwd = explode(DIRECTORY_SEPARATOR, getcwd());
-      // get parent directory of every file and folder
-      $parent_directory = DIRECTORY_SEPARATOR . $cwd[sizeof($cwd) - 1] . DIRECTORY_SEPARATOR;
-      echo '<h4>parent directory: <span class="directory-info">' . $parent_directory . '</span></h4><br>';
-      echo '<h4>cwd different than virtualhost</h4><br>';
-    } else {
-      // store absolute path as a single value array
-      $cwd = array(getcwd());
-      // add a slash
-      $parent_directory = DIRECTORY_SEPARATOR;
-      echo '<h4>cwd same as virtualhost</h4><br>';
-    }
-    echo '</div></section>';
-    /* --------------------------------- */
-
-    /* ----------- ITERATE OVER CURRENT DIRECTORY ----------- */
+    echo '<section><div class="content-container">';
     // get current working directory
     $cwd_content = scandir(getcwd());
-
-    echo '<section><div class="content-container">';
-    // iterate over directory
+    // iterate over current working directory
     foreach ($cwd_content as $item) {
-      $item_name = preg_replace('/\\.[^.\\s]{3,4}$/', '', $item);
-      $file_type = str_replace('/', ' ', mime_content_type(realpath($item)));
+      $item_name = preg_replace('/\\.[^.\\s]{3,4}$/', '', $item); // get item name
+      $file_type = str_replace('/', ' ', mime_content_type(realpath($item))); // get file type
       // check whether the item is a directory
       if (is_dir(realpath($item))) {
+        // add <form> tag with post method for changing directory
         if ($item === '.') {
-          // add form tag with post method
-          echo '<form method="post" action="" enctype="application/x-www-form-urlencoded">';
-          echo '<input type="hidden" name="selected_item" value="' . realpath($item) . '">';
-          echo '<a class="fa fa-folder-open-o" href="' . $parent_directory . $item . '">';
-          echo '<button type="submit">' . $item_name . '</button>';
+          echo '<form method="post" enctype="application/x-www-form-urlencoded">';
+          echo "<input type=\"hidden\" name=\"selected_item\" value=\"" . realpath($item) . "\">";
+          echo "<a class=\"fa fa-folder-open-o\">";
+          echo "<button type=\"submit\">$item_name</button>";
           echo '</a>';
-          echo '</form>' . '<br>';
+          echo '</form><br>';
         } else {
-          echo '<form method="post" action="" enctype="application/x-www-form-urlencoded">';
-          echo '<input type="hidden" name="selected_item" value="' . realpath($item) . '">';
-          echo '<a class="fa fa-folder-o" href="' . $parent_directory . $item . '">';
-          echo '<button type="submit">' . $item_name . '</button>';
+          echo '<form method="post" enctype="application/x-www-form-urlencoded">';
+          echo "<input type=\"hidden\" name=\"selected_item\" value=\"" . realpath($item) . "\">";
+          echo "<a class=\"fa fa-folder-o\">";
+          echo "<button type=\"submit\">$item_name</button>";
           echo '</a>';
-          echo '</form>' . '<br>';
+          echo '</form><br>';
         }
         // or a file
       } elseif (is_file(realpath($item))) {
+        $item = DIRECTORY_SEPARATOR . $item;
+        // add <anchor> tag to open file
         if (strpos($file_type, 'image') !== false) {
-          // add anchor tag to open file
-          echo '<a class="fa fa-file-image-o file" href="' . $parent_directory . $item . '"><button type="submit">' . $item_name . '</button></a>' . '<br>';
+          echo "<a class=\"fa fa-file-image-o file\" href=\"${url}${item}\">";
+          echo "<button type=\"submit\">$item_name</button>";
+          echo '</a><br>';
         } elseif (strpos($file_type, 'text') !== false) {
-          echo '<a class="fa fa-file-text-o file" href="' . $parent_directory . $item . '"><button type="submit">' . $item_name . '</button></a>' . '<br>';
+          echo "<a class=\"fa fa-file-text-o file\" href=\"${url}${item}\">";
+          echo "<button type=\"submit\">$item_name</button>";
+          echo '</a><br>';
         } elseif (strpos($file_type, 'audio') !== false) {
-          echo '<a class="fa fa-file-sound-o file" href="' . $parent_directory . $item . '"><button type="submit">' . $item_name . '</button></a>' . '<br>';
+          echo "<a class=\"fa fa-file-sound-o file\" href=\"${url}${item}\">";
+          echo "<button type=\"submit\">$item_name</button>";
+          echo '</a><br>';
         } else {
-          echo '<a class="fa fa fa-file-o file" href="' . $parent_directory . $item . '"><button type="submit">' . $item_name . '</button></a>' . '<br>';
+          echo "<a class=\"fa fa-file-o file\" href=\"${url}${item}\">";
+          echo "<button type=\"submit\">$item_name</button>";
+          echo '</a><br>';
         }
       }
     }
     echo '</div></section>';
     /* --------------------------------- */
 
-    /* ----------- MODAL ----------- */
-    echo "<div id=\"myModal\" class=\"modal\">";
+    /* ----------- MODAL TEST ----------- */
+    echo "<section><div id=\"myModal\" class=\"modal\">";
     echo "<span id=\"close\" class=\"go-to-parent-directory\">&times;</span>";
-    echo "<div class=\"display-content\">";
+    echo "<div class=\"modal-content\">";
     echo "<div class=\"slide\">";
     echo "<img src=\"intro-bg.jpg\" class=\"file test-img\">";
     echo "</div>";
     echo "</div>";
-    echo "</div>";
+    echo "</div></section>";
     /* --------------------------------- */
-
     echo '</main>';
 
 
@@ -146,6 +156,6 @@
 
     ?>
 
-    <script type="text/javascript" src="script.js"></script>
+    <!-- <script type="text/javascript" src="script.js"></script> -->
   </body>
 </html>
--- script.js
+++ script.js
@@ -3,16 +3,34 @@
 // slide index for lightbox
 let slideIndex = 1;
 
-// close button
-const closeButton = () => {
+// close modal
+const closeModal = () => {
   const close = document.getElementById("close");
   close.addEventListener("click", e => {
     e.preventDefault();
-    console.log(close);
+    console.log('modal closed');
     document.getElementById("myModal").style.display = "none";
   });
 }
-console.log(closeButton());
+closeModal();
+
+// get all <a> tags containing files and open modal
+const openModal = () => {
+  const files = document.querySelectorAll('[class*="file"]');
+  console.log(files);
+  for (let n in files) {
+    if (files.hasOwnProperty(n)) {
+      files[n].addEventListener("click", e => {
+        e.preventDefault();
+        console.log('modal opened');
+        document.getElementById("myModal").style.display = "block";
+        currentSlide(files);
+      });
+    }
+  }
+}
+// call function
+openModal();
 
 // display first modal slide
 showSlides(slideIndex);
@@ -26,36 +44,25 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
-// get all <a> tags containing files
-const displayFile = () => {
-  const files = document.querySelectorAll('[class*="file"]');
-  console.log(files);
-  for (let n in files) {
-    if (files.hasOwnProperty(n)) {
-      files[n].addEventListener("click", e => {
-        e.preventDefault();
-        // alert("clicked!");
-        document.getElementById("myModal").style.display = "block";
-        currentSlide(files);
-      });
-    }
-  }
-}
-// call function
-displayFile();
-
 // keyboard control
 function logKey(e) {
   if (keys[event.keyCode] !== undefined) {
+    console.log(e);
     if (keys[event.keyCode] === "esc") {
-      closeModal();
+      console.log('modal closed');
+      document.getElementById("myModal").style.display = "none";
     }
   }
 }
 
-
 /* CLASSES AND OBJECTS */
-// keyboard keys
+
+// keyboard 'esc' key
 const keys = {
   27: "esc"
 }
+
+/* EVENT LISTENERS */
+
+// control modal with "left", "right" and "esc" keys
+document.addEventListener('keydown', logKey);
